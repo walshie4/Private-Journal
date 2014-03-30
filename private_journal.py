5774fb87d5ffbb3fc49a907c742e3d9ed6b7bf50
Adam Walsh <adamwalsh14@gmail.com>
2014-03-30 14:45:13 -0400

    Added padding and un-padding thanks to @kisom's pycrypto tutorial, as well as the licence for his code

--- private_journal.py
+++ private_journal.py
@@ -7,11 +7,32 @@
 from Crypto.Cipher import AES
 import os
 
-LOCATION = '/usr/bin/'
+LOCATION = '/usr/bin/' #location where the journal is stored
 
-if os.path.isfile(LOCATION): #journal already exists
-    os.system("touch " + LOCATION) #create new journal
+if __name__ == "__main__":
+    if os.path.isfile(LOCATION): #journal already exists
+        os.system("touch " + LOCATION) #create new journal
 
-key = input("Please enter your journals encryption passphrase.")
-cipher = AES.new(key)
+    key = input("Please enter your journals encryption passphrase.")
+    cipher = AES.new(key)
+
+def pad_data(data): #add padding to make data divisible by 16 byte (128-bit) blocks
+    if len(data) % 16 == 0: #no padding needed
+        return data
+    padding_required = 15 - (len(data) % 16) #this function is taken from @kisom's cytpo tutorial
+                                             #https://github.com/kisom/crypto_intro
+    data = '%s\x80' % data                   #See licence for this and following function
+    data = '%s%s' % (data, '\x00' * padding_required) #titled 'kisom's Licence'
+    return data
+
+def unpad_data(data): #remove padding to original state
+    if not data:
+        return data
+
+    data = data.rstrip('\x00')
+    if data[-1] == '\x80':
+        return data[:-1]
+    else:
+        return data
+
 
