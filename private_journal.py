af6a429999058a48fec05debc4d578f3609fd279
Adam Walsh <adamwalsh14@gmail.com>
2014-04-21 03:29:00 -0400

    Added logic to delete unencrypted journal and replace it with encrypted tmp journal. Also added warning notice at beginning of execution

--- private_journal.py
+++ private_journal.py
@@ -9,8 +9,13 @@ import encrypt#import encrypt.py
 import sys
 import fileinput
 import time
+from shutil import move
 
 if __name__ == "__main__":
+    print("WARNING: Killing execution at any point, (or encountering an error) may cause your journal\n"
+        + "to be left on your drive unencrypted. If the program exits before stating the journal was\n"
+        + "re-encrypted please check to see if it encrypted. If an error caused the exit please report\n"
+        + "it so I can fix it ASAP. Sorry for any inconvience, hope you enjoy this script\n")
     location = input("Please enter the location of the journal (parent directory containing journal,\n"
                     +"it will be created if it does not exist\n-> ")
     encrypted = True
@@ -32,6 +37,8 @@ if __name__ == "__main__":
         out.write(time.strftime("%c") + "\n\n")
         out.write(entry + "\n")
     print("Encrypting journal...(Please wait)")
-    encrypt.main(input_file, "e", 1000000, location + "temp")#re-encrypt journal
+    encrypt.main(input_file, "e", 1000000, location + ".tmp")#re-encrypt journal
+    os.remove(location)
+    move(location + ".tmp", location)#replace location(unencrypted) with location.tmp(encrypted)
     print("Exiting...")
 
