5cea6756896ec49f12971dd283c06dc7a81ea5a8
Adam Walsh <adamwalsh14@gmail.com>
2014-12-27 22:06:14 -0500

    Fairly major re-write to move to shell script using openssl and a dedicated dir for entries each titled by timestamp.

--- privJournal.sh
+++ privJournal.sh
@@ -1,17 +1,34 @@
 #!/usr/bin/env sh
-NAME=$1
+BASELOC="$HOME/privJournal"
+ENTRYLOC="$BASELOC/entries"
 #check for param
-if [[ -z "$NAME" ]]; then
-    echo "Usage: privJournal <filename>"
-    exit;
+echo "\nPrivJournal -- A simple encrypted journal utility.\n"
+if ! [[ -e "$ENTRYLOC" ]]; then
+    if ! [[ -e "$BASELOC" ]]; then
+        mkdir "$BASELOC"
+    fi
+    mkdir "$ENTRYLOC"
 fi
-echo "PrivJournal\n\n"
-if [[ -e "$NAME" ]]; then
-    echo "Journal found."
-else
-    echo "Creating journal."
-    touch "$NAME"
-fi
-echo "Input journal password and entry."
-openssl des3 -salt -out "$NAME"
-openssl des3 -d -salt -in "$NAME"
+CMD=$1
+case "$CMD" in
+    "Read" | "READ" | "read") #read
+        ls "$ENTRYLOC"
+        echo -n "\nEnter name of the entry you would like to read.\n-> "
+        read NAME
+        openssl des3 -d -salt -in "$ENTRYLOC/$NAME";;
+    "make" | "Make" | "MAKE" | "new" | "NEW" | "New" | "mk") #make
+        NAME=`date "+%Y-%m-%d-%H-%M-%S"`
+        NAME="$ENTRYLOC/$NAME"
+        touch "$NAME"
+        echo "Input journal password, confirm, and then input entry."
+        openssl des3 -salt -out "$NAME";;
+    "list" | "ls" | "LIST" | "List")
+        ls "$ENTRYLOC";;
+    *) #no command given, show usage
+        echo "Usage: privJournal [make | read | list]\n"
+        echo "Make - Make a new entry"
+        echo "Read - Read a past entry"
+        echo "List - List all past entries\n"
+        echo "If you have any feature requests, or run into any"
+        echo "issues please let me know on the project's Github page.";;
+esac
